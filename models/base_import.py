fe748dc60455875486ce5b0eca2b0365d73a200a
Gary <kaiyuewang123@gmail.com>
2019-07-23 03:46:42 +0000

    added inception model

--- models/base_import.py
+++ models/base_import.py
@@ -1,6 +1,8 @@
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Activation, Lambda
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Activation, Lambda, Input, GlobalAveragePooling2D
 from keras.layers.normalization import BatchNormalization
+from keras.applications.inception_v3 import InceptionV3
+import tensorflow as tf
 
 def preprocess_model():
     model = Sequential()
@@ -16,51 +18,45 @@ def simple_mode():
 
 def alex_net():
     model = preprocess_model()
-    model.add(Conv2D(filters=96, kernel_size=(11, 11), strides=(2, 2), padding="same", name="conv1"))
+    model.add(Conv2D(32, (3, 3), padding='same'))
     model.add(Activation('relu'))
-    # Max Pooling
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-
-    # 2nd Convolutional Layer
-    model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid', name="conv2"))
+    model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
-    # Max Pooling
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # 3rd Convolutional Layer
-    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', name="conv3"))
+    model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(Activation('relu'))
-
-    # 4th Convolutional Layer
-    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', name="conv4"))
+    model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # 5th Convolutional Layer
-    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid', name="conv5"))
-    model.add(Activation('relu'))
-    # Max Pooling
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-
-    # Passing it to a Fully Connected layer
     model.add(Flatten())
-    # 1st Fully Connected Layer
-    model.add(Dense(4096))
+    model.add(Dense(512))
     model.add(Activation('relu'))
-    # Add Dropout to prevent overfitting
-    model.add(Dropout(0.4))
-
-    # 2nd Fully Connected Layer
-    model.add(Dense(4096))
-    model.add(Activation('relu'))
-    # Add Dropout
-    model.add(Dropout(0.4))
-
-    # 3rd Fully Connected Layer
-    model.add(Dense(1000))
-    model.add(Activation('relu'))
-    # Add Dropout
-    model.add(Dropout(0.4))
-
+    model.add(Dropout(0.5))
     # Output Layer
     model.add(Dense(1))
     return model
+
+
+def inception_v3():
+#     model = preprocess_model()
+    image_input = Input(shape=(160, 320, 3))
+    resized_input = Lambda(lambda image: tf.image.resize_images(image, (139, 139)))(image_input)
+#     normalized_input = Lambda(lambda x: x / 255.0 - 0.5)(resized_input)
+    
+    input_size = 139
+
+    # Using Inception with ImageNet pre-trained weights
+    inception = InceptionV3(weights='imagenet', include_top=False,input_shape=(input_size,input_size,3))
+    
+    inp = inception(resized_input)
+    x = GlobalAveragePooling2D()(inp)
+    x = Dense(512, activation='relu')(x)
+    predictions = Dense(1)(x)
+    return Model(inputs=image_input, outputs=predictions)
+    
+    
+    
--- train.py
+++ train.py
@@ -4,13 +4,14 @@ import utility as util
 
 
 def train(label, image):
-    model = models.alex_net()
+    model = models.inception_v3()
+    print (model.summary())
     model.compile(loss='mse', optimizer='adam')
-    model.fit(image, label, validation_split=0.2, shuffle=True, epochs=20)
-    model.save(util.model_save_path('alex_net.h5'))
+    model.fit(image, label, validation_split=0.2, shuffle=True, epochs=3)
+    model.save(util.model_save_path('inception_v3.h5'))
 
 
 if __name__ == '__main__':
-    image, label = util.parse_drive_log("data/driving_log.csv")
+    image, label = util.parse_drive_log("../CarND-Behavioral-Cloning-P3/data/driving_log.csv")
     print (image[0].shape)
     train(label, image)
